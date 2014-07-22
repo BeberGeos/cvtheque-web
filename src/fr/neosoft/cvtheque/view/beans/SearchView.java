20805749cb4164bef9006a04a8bba16a22f08790
Adrien Cambillau <adrien.cambillau@inge.neo-soft.fr>
2014-07-22 17:50:44 +0200

    Affichage des utilisateurs et filtres

---
+++ src/fr/neosoft/cvtheque/view/beans/SearchView.java
@@ -0,0 +1,56 @@
+package fr.neosoft.cvtheque.view.beans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import org.primefaces.model.StreamedContent;
+
+import fr.neosoft.cvtheque.dao.UtilisateurDao;
+import fr.neosoft.cvtheque.entities.Utilisateur;
+
+@ManagedBean(name="dtSearchView")
+@ViewScoped
+public class SearchView implements Serializable {
+	
+	private static final long serialVersionUID = 6084890189853253103L;
+	
+	private List<Utilisateur> users;
+	private Utilisateur selectedUser;
+
+	@ManagedProperty("#{utilisateurDao}")
+	private UtilisateurDao userDao;
+	
+	@PostConstruct
+    public void init() {
+        users = userDao.findAllUtilisateurs();
+    }
+	
+	public List<Utilisateur> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<Utilisateur> users) {
+		this.users = users;
+	}
+
+	public Utilisateur getSelectedUser() {
+		return selectedUser;
+	}
+
+	public void setSelectedUser(Utilisateur selectedUser) {
+		this.selectedUser = selectedUser;
+	}
+
+	public UtilisateurDao getUserDao() {
+		return userDao;
+	}
+
+	public void setUserDao(UtilisateurDao userDao) {
+		this.userDao = userDao;
+	}
+}
